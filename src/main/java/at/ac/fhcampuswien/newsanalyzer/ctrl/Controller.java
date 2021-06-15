a496c05e806debe804ab181d06ebfcda6c0d9f32
Zeynep-Avci <zeynep.avci.66@hotmail.com>
2021-06-15 20:18:24 +0200

    Aufgabe 3 wurde gemacht

--- src/main/java/at/ac/fhcampuswien/newsanalyzer/ctrl/Controller.java
+++ src/main/java/at/ac/fhcampuswien/newsanalyzer/ctrl/Controller.java
@@ -1,15 +1,41 @@
 package at.ac.fhcampuswien.newsanalyzer.ctrl;
 
+import at.ac.fhcampuswien.newsapi.NewsApi;
+import at.ac.fhcampuswien.newsapi.NewsApiBuilder;
+import at.ac.fhcampuswien.newsapi.beans.Article;
+import at.ac.fhcampuswien.newsapi.beans.NewsResponse;
+import at.ac.fhcampuswien.newsapi.enums.Category;
+import at.ac.fhcampuswien.newsapi.enums.Country;
+import at.ac.fhcampuswien.newsapi.enums.Endpoint;
+import at.ac.fhcampuswien.newsapi.enums.Language;
+
+import java.util.List;
+
 public class Controller {
 
 	public static final String APIKEY = "428ce1e3cf64410ebde5dcd05cd4d3e9";  //TODO add your api key
 
-	public void process() {
+	public void process(String apiKey, String q, Endpoint endpoint, Country sourceCountry, Category sourceCategory, Language language) {
 		System.out.println("Start process");
 
 		//TODO implement Error handling
 
 		//TODO load the news based on the parameters
+
+		NewsApi newsApi = new NewsApiBuilder()
+				.setApiKey(apiKey)
+				.setQ(q)
+				.setEndPoint(endpoint)
+				.setSourceCountry(sourceCountry)
+				.setSourceCategory(sourceCategory)
+				.setLanguage(language)
+				.createNewsApi();
+
+		NewsResponse newsResponse = newsApi.getNews();
+		if(newsResponse != null){
+			List<Article> articles = newsResponse.getArticles();
+			articles.stream().forEach(article -> System.out.println(article.toString()));
+		}
 
 		//TODO implement methods for analysis
 
--- src/main/java/at/ac/fhcampuswien/newsanalyzer/ui/UserInterface.java
+++ src/main/java/at/ac/fhcampuswien/newsanalyzer/ui/UserInterface.java
@@ -2,40 +2,62 @@ package at.ac.fhcampuswien.newsanalyzer.ui;
 
 
 import at.ac.fhcampuswien.newsanalyzer.ctrl.Controller;
+import at.ac.fhcampuswien.newsapi.NewsApi;
+import at.ac.fhcampuswien.newsapi.NewsApiBuilder;
+import at.ac.fhcampuswien.newsapi.enums.Category;
+import at.ac.fhcampuswien.newsapi.enums.Country;
+import at.ac.fhcampuswien.newsapi.enums.Endpoint;
+import at.ac.fhcampuswien.newsapi.enums.Language;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class UserInterface 
 {
 	private Controller ctrl = new Controller();
 
 	public void getDataFromCtrl1(){
-		System.out.println("ABC");
+		System.out.println("Articles in sports with keyword 'ball'");
 
-		ctrl.process();
+		ctrl.process("428ce1e3cf64410ebde5dcd05cd4d3e9", "ball", Endpoint.TOP_HEADLINES, Country.at, Category.sports, Language.de);
 	}
 
 	public void getDataFromCtrl2(){
 		// TODO implement me
+		System.out.println("Articles in business with keyword 'money'");
+
+		ctrl.process("428ce1e3cf64410ebde5dcd05cd4d3e9", "money", Endpoint.TOP_HEADLINES, Country.at, Category.business, Language.de);
 	}
 
 	public void getDataFromCtrl3(){
 		// TODO implement me
+		System.out.println("Articles in health with keyword 'corona'");
+
+		ctrl.process("428ce1e3cf64410ebde5dcd05cd4d3e9", "corona", Endpoint.TOP_HEADLINES, Country.at, Category.health, Language.de);
 	}
 	
 	public void getDataForCustomInput() {
 		// TODO implement me
+		System.out.println("User Input");
+		Scanner scanner = new Scanner(System.in);
+		String ScanQ = scanner.next();
+		Endpoint ScanEndPoint = Endpoint.valueOf(scanner.next());
+		Country ScanCountry = Country.valueOf(scanner.next());
+		Category ScanCategory = Category.valueOf(scanner.next());
+		Language ScanLanguage = Language.valueOf(scanner.next());
+
+		ctrl.process("428ce1e3cf64410ebde5dcd05cd4d3e9",ScanQ,ScanEndPoint, ScanCountry, ScanCategory, ScanLanguage );
 	}
 
 
 	public void start() {
 		Menu<Runnable> menu = new Menu<>("User Interface");
 		menu.setTitle("Wählen Sie aus:");
-		menu.insert("a", "Choice ABC", this::getDataFromCtrl1);
-		menu.insert("b", "Choice DEF", this::getDataFromCtrl2);
-		menu.insert("c", "Choice 3", this::getDataFromCtrl3);
+		menu.insert("a", "Articles in sports with keyword 'ball'", this::getDataFromCtrl1);
+		menu.insert("b", "Articles in business with keyword 'money'", this::getDataFromCtrl2);
+		menu.insert("c", "Articles in health with keyword 'corona'", this::getDataFromCtrl3);
 		menu.insert("d", "Choice User Input:",this::getDataForCustomInput);
 		menu.insert("q", "Quit", null);
 		Runnable choice;
