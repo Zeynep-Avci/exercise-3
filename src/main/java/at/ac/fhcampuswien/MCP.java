0638f3e0551f16cd729439432a7ea67f780c981b
Zeynep-Avci <zeynep.avci.66@hotmail.com>
2021-06-16 12:55:51 +0200

    names

--- src/main/java/at/ac/fhcampuswien/MCP.java
+++ src/main/java/at/ac/fhcampuswien/MCP.java
@@ -1,7 +1,7 @@
 package at.ac.fhcampuswien;
 
 import at.ac.fhcampuswien.newsanalyzer.ui.UserInterface;
-
+//worked with Greta Berdynaj, Nur Sarhan and Parsa Salvatian
 public class MCP {
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
